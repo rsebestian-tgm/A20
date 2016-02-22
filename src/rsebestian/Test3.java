66c5f6e9816f359bc8658909aeb1d3af55007920
Rene <renesebestian90@gmail.com>
2016-02-22 11:33:34 +0100

    Fehlerbehebung da ein (expeted=GEwichtException.class) fehlte.

--- src/rsebestian/Test3.java
+++ src/rsebestian/Test3.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 //Offenbar ist in GEWICHT ein Formatierungsfehler weil das eigengewicht nicht zum gesamtgewicht zählt.
 import weiser.*;
 public class Test3 {
-	@Test
+	@Test(expected=GewichtException.class)
 	public void testlast(){
 		LKW johann = new LKW("J0H4NN",5000,1300,3);
 		johann.aufladen(2000);
--- src/rsebestian/TestSuite1.java
+++ src/rsebestian/TestSuite1.java
@@ -1,8 +1,8 @@
 package rsebestian;
-
+//ssh: git@github.com:rsebestian/A20.git
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({Test1.class,Test2.class,Test3.class})
+@Suite.SuiteClasses({Test1.class,Test2.class,Test3.class,Test4.class})
 public class TestSuite1 { }
